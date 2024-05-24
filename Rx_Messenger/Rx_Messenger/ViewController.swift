21d96ba6d462f59d38447fe7450215f3b809e146
khanhnvm-macbook <>
2024-05-24 10:45:09 +0700

    Init project

---
+++ Rx_Messenger/Rx_Messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rx_Messenger
+//
+//  Created by khanhnvm-macbook on 24/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
